b41c9f7119c4213f83075f16e23ac689578a6554
Rosario-Daune <110370299+Rosario-Daune@users.noreply.github.com>
2022-11-08 06:00:32 -0300

    Add files via upload

---
+++ sugerencias.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sugerencias.</title>
+</head>
+<body>
+     <form action="sugerencias.php" method="post">
+         <table width="100%", border=0, cellpadding="2", cellspacing="2">
+             <br>
+             <tr>
+                 <td></td>
+             </tr>    
+             <tr>
+                 <td> Nombre: </td>
+                 <td><input name="nomb" size="35" maxlength="50" type="text"></td>
+             </tr>
+             <tr>  
+                 <td> Aoellido: </td>
+                 <td><input name="apel" size="35" maxlength="50" type="text"></td>
+             </tr>  
+             <tr>  
+                 <td> Email: </td>
+                 <td><input name="mail" size="35" maxlength="50" type="text"></td>
+             </tr>  
+             <tr>  
+                 <td> Mensaje: </td>
+                 <td><textarea name="mensage" cols="50" rows="5" wrap="soft"></td>
+             </tr>  
+             <td></td>
+</body>
+</html>
